4b595cdbc997c75611cd9cc514ef442bc56ec0b3
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-11-02 11:37:26 +0100

    Add welcome text

--- src/App.js
+++ src/App.js
@@ -4,7 +4,23 @@ function App() {
   return (
     <div className="App">
       <header className="app-header">proto-chat</header>
-      <main className="app-content"></main>
+      <main className="app-content">
+        <h1>Welcome to proto-chat !</h1>
+        <p>
+          It is a tiny chat app built to test google{" "}
+          <a
+            href="https://firebase.google.com/"
+            target="blank"
+            rel="noreferrer noopener"
+          >
+            firebase
+          </a>
+        </p>
+        <p>
+          Sign in using a google account to start chatting with everyone using
+          this app. <small>That means not a lot of people actually 😉</small>
+        </p>
+      </main>
     </div>
   );
 }
