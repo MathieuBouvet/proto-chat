aa5b8bca20dd65ff3881fd03d247e0d07d108cbb
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-11-03 10:51:20 +0100

    Set a user as offline when quitting the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { useEffect } from "react";
 import Topbar from "./components/Topbar";
 import Landing from "./components/Landing";
 import UserProvider, { UserContext } from "./UserProvider";
+import { signOut } from "./services/firebase";
 
 import "./App.scss";
 
 function App() {
+  useEffect(() => {
+    const beforeUnload = async () => {
+      await signOut();
+    };
+    window.addEventListener("beforeunload", beforeUnload);
+    return () => window.removeEventListener("beforeunload", beforeUnload);
+  }, []);
   return (
     <UserProvider>
       <UserContext.Consumer>
