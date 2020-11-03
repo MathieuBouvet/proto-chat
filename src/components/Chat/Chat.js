07f639bd135a05a2cc4281ba23b821f9ae643cff
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-11-03 11:54:19 +0100

    Create basic user listing

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,14 +1,37 @@
-import { useContext } from "react";
+import { useState, useEffect } from "react";
 
-import { UserContext } from "../../UserProvider";
+import { db } from "../../services/firebase";
 
 import "./Chat.scss";
 
 const Chat = () => {
-  const { user } = useContext(UserContext);
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const usersRef = db.ref("users");
+    usersRef.on("value", (snapshot) => {
+      const users = [];
+      snapshot.forEach((child) => {
+        users.push({ ...child.val(), uid: child.key });
+      });
+      setUsers(users);
+    });
+
+    return () => usersRef.off();
+  }, []);
   return (
     <main className="Chat">
-      <section className="users"></section>
+      <section className="users">
+        <h3>Users</h3>
+        <ul>
+          {users.map((user) => (
+            <li key={user.uid}>
+              {user.online ? "online" : "offline"}
+              {" - "}
+              {user.name}
+            </li>
+          ))}
+        </ul>
+      </section>
     </main>
   );
 };
